b56c6538a51fef22ef5392f1a08ac480025ee71e
temptado <jason1287712@gmail.com>
2022-07-05 13:41:41 -0400

    added function to create active link sytling on title

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@ window.addEventListener('scroll', scrollHeader);
 
 let mixerProducts = mixitup('.products__content', {
     selectors: {
-        target: '.products__card'
+        target: '.products__card '
     },
     animation: {
         duration: 300
@@ -56,8 +56,19 @@ let mixerProducts = mixitup('.products__content', {
 
 /* Default filter products */
 
+mixerProducts.filter('.delicacies');
 
 /* Link active products */
+
+const linkProducts = document.querySelectorAll('.products__item');
+
+function activeProducts() {
+    linkProducts.forEach(l => l.classList.remove('active-product'));
+    this.classList.add('active-product');
+}
+
+linkProducts.forEach(l => l.addEventListener('click', activeProducts));
+
 
 
 /*=============== SHOW SCROLL UP ===============*/
